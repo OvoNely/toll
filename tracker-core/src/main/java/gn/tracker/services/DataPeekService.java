9f6c1f710fa13b19824392236541ab11930587ba
OvoNely <lisper@mai.ru>
2019-04-13 15:00:22 +0700

    иньекция зависимости

--- tracker-core/src/main/java/gn/tracker/services/DataPeekService.java
+++ tracker-core/src/main/java/gn/tracker/services/DataPeekService.java
@@ -1,7 +1,11 @@
 package gn.tracker.services;
 
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Service
 public class DataPeekService {
+
+    @Autowired
+    private DataSendService dataSendService;
 }
