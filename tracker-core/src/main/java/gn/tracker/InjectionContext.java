f6df78f6010a1e730710aa2db6d841590d453e50
OvoNely <lisper@mai.ru>
2019-04-13 15:13:12 +0700

    кофигурирование бинов + postconstruct

--- tracker-core/src/main/java/gn/tracker/InjectionContext.java
+++ tracker-core/src/main/java/gn/tracker/InjectionContext.java
@@ -1,7 +1,20 @@
 package gn.tracker;
 
 import org.springframework.context.annotation.Configuration;
+import gn.tracker.services.DataPeekService;
+import gn.tracker.services.DataSendService;
+import org.springframework.context.annotation.Bean;
 
 @Configuration
 public class InjectionContext {
+
+    @Bean
+    public DataPeekService peekService() {
+        return new DataPeekService();
+    }
+
+    @Bean
+    public DataSendService sendService() {
+        return new DataSendService();
+    }
 }
--- tracker-core/src/main/java/gn/tracker/services/DataPeekService.java
+++ tracker-core/src/main/java/gn/tracker/services/DataPeekService.java
@@ -2,10 +2,16 @@ package gn.tracker.services;
 
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
+import javax.annotation.PostConstruct;
 
 @Service
 public class DataPeekService {
 
     @Autowired
     private DataSendService dataSendService;
+
+    @PostConstruct
+    private void init() {
+        dataSendService.callFromInit();
+    }
 }
